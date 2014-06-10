4cc7f8b0b75259f3257fcff6379036e30bf71cf3
John Smith <private@unknown.com>
2014-06-09 18:36:00 -0700

    Displays website table and displays fields

--- app/controllers/render_site_controller.rb
+++ app/controllers/render_site_controller.rb
@@ -1,5 +1,10 @@
 class RenderSiteController < ApplicationController
   def index
-		@website_content = ['render head','render content','render tail']
+		current_site = Website.find(params[:id])
+		@website_content = []
+		@website_content << current_site.top_content
+		@website_content << current_site.bottom_content
+
+		#@website_content = ['render head','render content','render tail']
   end
 end
